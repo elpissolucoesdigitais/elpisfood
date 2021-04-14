696b77abc550ce436e51a7c5f48c1bc7542d9ce4
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-04-14 16:25:33 -0400

    CRIAR TABELA E MODEL DE TENANTS E ORGANIZAR MIGRATIONS

---
+++ elpisfood/database/migrations/2014_04_14_195500_create_tenants_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cnpj')->unique();
+            $table->string('name')->unique();
+            $table->string('url')->unique();
+            $table->string('email')->unique();
+            $table->string('logo')->nullable();
+            $table->unsignedBigInteger('plan_id');
+            $table->foreign('plan_id')->references('id')->on('plans');
+            //status tenant (se inativar 'N' ele perde o acesso ao sistema)
+            $table->enum('active',['Y','N'])->default('Y');
+
+            //subscription
+            $table->date('subscription')->nullable();//data que se escreveu
+            $table->date('expires_at')->nullable();//data que expira o acesso
+            $table->string('subscription_id',255)->nullable();//identificador do gateway de pagamento
+            $table->boolean('subscription_active')->default(false);//assinatura ativa
+            $table->boolean('subscription_suspended')->default(false);//assinatura cancelada
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
--- elpisfood/database/migrations/2014_10_12_000000_create_users_table.php
+++ elpisfood/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,11 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('tenant_id');
+            $table->foreign('tenant_id')
+                            ->references('id')
+                            ->on('tenants')
+                            ->onDelete('cascade');
         });
     }
 
