d0e94cbc699eb8b65cf23c69102e2e80cc67bb09
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-04-15 11:41:02 -0400

    validações de cadastro tenant

--- elpisfood/app/Http/Controllers/Auth/RegisterController.php
+++ elpisfood/app/Http/Controllers/Auth/RegisterController.php
@@ -51,11 +51,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'min:3','max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'cnpj'=>['required','unique:tenants'],
-            'empresa'=>['required','unique:tenants,name']
+            'password' => ['required', 'string', 'min:6','max:16', 'confirmed'],
+            'cnpj'=>['required','numeric','min:14','unique:tenants'],
+            'empresa'=>['required','string','min:3','max:255','unique:tenants,name']
         ]);
     }
 
