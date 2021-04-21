a9bf0979c1a342f293630501e89cdc12be99d87f
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-04-21 09:48:22 -0400

    LARAVEL ACL - RECUPERAR AS PERMISSÕES DO USUÁRIO

--- elpisfood/app/Models/Traits/UserACLTrait.php
+++ elpisfood/app/Models/Traits/UserACLTrait.php
@@ -2,13 +2,32 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Tenant;
+
 trait UserACLTrait
 {
     public function permissions()
     {
-        $tenant = $this->tenant()->first();
-        $plan =$tenant->plan;
+        $permissionPlan = $this->permissionsPlan();
+        $permissionRole = $this->permissionsRole();
 
+        $permissions = [];
+
+        foreach ($$permissonsRole as $permissionRole) {
+            if(in_array($permissionRole, $permissionPlan)){
+                array_push($permissions,$permissionsPlan);
+            }
+
+        }
+        return $permissions;
+    }
+    public function permissionsPlan():array
+    {
+        //$tenant = $this->tenant()->first();
+        //$plan =$tenant->plan;
+
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id',$this->tenant_id)->first();
+        $plan = $tenant->plan;
         $permissions = [];
         foreach($plan->profiles as $profile)
         {
@@ -21,14 +40,30 @@ trait UserACLTrait
 
         return $permissions;
     }
+    public function permissionsRole():array
+    {
+        $roles = $this->roles()->with('permissions')->get();
+
+        $permissions = [];
+        foreach($roles->permissions as $permission){
+            array_push($permissions, $permission->name);
+        }
+        return $permissions;
+    }
+
+
     public function hasPermission(string $permissionName):bool
     {
         return in_array($permissionName, $this->permissions());
     }
+
+
     public function isAdmin():bool
     {
         return in_array($this->email, config('acl.admins'));
     }
+
+
     public function isTenant():bool
     {
         return !in_array($this->email, config('acl.admins'));
