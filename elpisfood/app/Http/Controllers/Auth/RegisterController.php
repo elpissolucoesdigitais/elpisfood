5e8b6562581a6eef1a0edff979f03f62540aebf1
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-04-21 10:21:22 -0400

    evento no laravel

--- elpisfood/app/Http/Controllers/Auth/RegisterController.php
+++ elpisfood/app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use App\Services\TenantService;
+use App\Tentant\Events\TenantCreated;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -75,6 +76,7 @@ class RegisterController extends Controller
         $tenantService = app(TenantService::class);
         $user=$tenantService->make($plan,$data);
 
+        event(new TenantCreated($user));
         return $user;
     }
 }
--- elpisfood/app/Providers/EventServiceProvider.php
+++ elpisfood/app/Providers/EventServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Listeners\{
+    AddRoleTenant
+
+    };
+use App\Tentant\Events\TenantCreated;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +23,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        TenantCreated::class =>[
+            AddRoleTenant:class,
+        ]
     ];
 
     /**
