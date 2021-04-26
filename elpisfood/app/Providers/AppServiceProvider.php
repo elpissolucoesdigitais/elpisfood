233dc6634af7621b46f6ece44c84c981c0e562dc
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-04-26 06:53:03 -0400

    ERVICE PROVIDER

--- elpisfood/app/Providers/AppServiceProvider.php
+++ elpisfood/app/Providers/AppServiceProvider.php
@@ -10,12 +10,7 @@ use App\Observers\{
     PlanObserver,TenantObserver,CategoryObserver,
     ProductObserver
 };
-use App\Repositories\Contracts\{
-    TenantRepositoryInterface
-    };
-use App\Repositories\{
-    TenantRepository
-    };
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,10 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-        TenantRepositoryInterface::class,
-        TenantRepository::class
-        );
+
     }
 
     /**
