cb6e29d9c34858ee357d70e6841ee400d231af61
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-05-03 14:34:48 -0400

    atualizar logo e favicon

--- elpisfood/config/adminlte.php
+++ elpisfood/config/adminlte.php
@@ -46,7 +46,7 @@ return [
     */
 
     'logo' => '<b>Élpis </b>Food',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'imgs/logo.jpeg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
