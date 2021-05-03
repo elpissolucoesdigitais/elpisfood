e978211cc504bd58ca1edae43ebf685a66588b7b
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-05-03 09:15:56 -0400

    TESTAR API DE TENANTS NO LARAVEL

--- elpisfood/tests/Feature/TenantTest.php
+++ elpisfood/tests/Feature/TenantTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tenant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,8 +16,40 @@ class TenantTest extends TestCase
      */
     public function testGetAllTenants()
     {
-        $response = $this->get('api/v1/tenants');
 
+        factory(Tenant::class,10)->create();
+
+        $response = $this->getJson('api/v1/tenants');
+        //$response->dump();
+        $response->assertStatus(200)
+                ->assertJsonCount(10,'data');
+    }
+
+    /**
+     * Test Get Error Single Tenants
+     *
+     * @return void
+     */
+    public function testErrorGetTenants()
+    {
+        $tenant = 'fake_value';
+
+        $response = $this->getJson("api/v1/tenants/{$tenant}");
+        //$response->dump();
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test Get Tenant by identify
+     *
+     * @return void
+     */
+    public function testGetTenantByIdentify()
+    {
+        $tenant = factory(Tenant::class)->create();
+
+        $response = $this->getJson("api/v1/tenants/{$tenant->uuid}");
+        //$response->dump();
         $response->assertStatus(200);
     }
 }
